62807d180aa0247712fdcbaa11432ed695b17c98
Martijn Hermans <martijn.hermans@gmail.com>
2012-11-12 17:47:12 -0500

    .

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 
-var express = require('express');
-var routes = require('./routes.js');
-var routeConnector = require('./route-connector.js');
-var authentication = require('./authentication.js');
+var express = require('express'),
+    routes = require('./routes.js'),
+    routeConnector = require('./route-connector.js'),
+    authentication = require('./authentication.js');
 
 var app = express();
 
-var fnLogin = function loginUser(accessLevel,username,password){
-    return (username === accessLevel && password === accessLevel);
-}
-
-routeConnector.connectToExpress(app,routes,express.basicAuth,authentication.authenticate);
-//routeConnector.connectToExpress(app,routes,express.basicAuth,fnLogin);
+routeConnector.connectToExpress(
+    app,
+    routes,
+    express.basicAuth,
+    authentication.authenticate);
 
 app.listen(process.env.PORT);
